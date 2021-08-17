c8e78363efb130afca1462833f6b00bd167d1481
LauSanMao <samuel_lau_sl@hotmail.com>
2021-08-17 13:53:59 +1000

    Added sayMyName File for Week 2

---
+++ sayMyName.py
@@ -0,0 +1,11 @@
+#This is a Program to print name N times. N equals first 3 digits of SID
+# SID : 470393701
+print("Type your name:")
+name = (input())
+
+
+print("Type first 3 digits of your SID:")
+N = (int(input()))
+
+for i in range(N):
+  print (name)
